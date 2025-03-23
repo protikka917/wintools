330eb8fec8f70a7f829b2c3041e56e56eee415ba
Abul Islam <protikka917@outlook.com>
2025-03-23 13:59:36 -0700

    Initiat commit

---
+++ wintools/MainForm.cs
@@ -0,0 +1,11 @@
+// See https://aka.ms/new-console-template for more information
+namespace wintools
+{
+    internal class MainForm : Form
+    {
+    }
+}
+
+
+
+
--- wintools/Program.cs
+++ wintools/Program.cs
@@ -1,5 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-namespace wintools;
+using System.Windows.Forms;
+
+namespace wintools
+{
+    internal static class NewBaseType
+    {
+        [STAThread]
+        static void Main()
+        {
+            if (OperatingSystem.IsWindowsVersionAtLeast(6, 1))
+            {
+                Application.SetCompatibleTextRenderingDefault(false);
+            }
+
+            // Run the MainForm
+            if (OperatingSystem.IsWindowsVersionAtLeast(6, 1))
+            {
+                Application.Run(new MainForm());
+            }
+        }
+    }
+
+
+
+}
+
+
+
 
 
